0c74262c91a6783830ddb69b2eb52ebfc8d936a7
Dennis Turco <dennisturco@gmail.com>
2025-09-25 16:50:16 +0200

    plotting png images

--- main.py
+++ main.py
@@ -12,17 +12,31 @@ if __name__ == "__main__":
     deltas_months = ManageDeal.deltas_time_months_for_all_deals(deals)
 
     avg_months = statistics.mean(deltas_months)
-    std_months = statistics.stdev(deltas_months) if len(deltas_months) > 1 else 0.5 # only if there are at least 2 values
+    std_months = statistics.stdev(deltas_months) if len(deltas_months) > 1 else 0.5
     print(f"Avg delta time (months): {avg_months:.2f}")
     print(f"Standard deviation (months): {std_months:.2f}")
 
     filtered_deltas = Utils.remove_outliers_iqr(deltas_months)
 
     avg_months = statistics.mean(filtered_deltas)
-    std_months = statistics.stdev(filtered_deltas) if len(filtered_deltas) > 1 else 0.5 # only if there are at least 2 values
-    print(f"Avg delta time (months) (outlier): {avg_months:.2f}")
-    print(f"Standard deviation (months) (outlier): {std_months:.2f}")
+    std_months = statistics.stdev(filtered_deltas) if len(filtered_deltas) > 1 else 0.5
+    print(f"Avg delta time (months) (outlier removed): {avg_months:.2f}")
+    print(f"Standard deviation (months) (outlier removed): {std_months:.2f}")
 
     plot = Plot()
-    plot.plot_std_data(deltas_months)
-    plot.plot_std_data(filtered_deltas)
+    plot.plot_std_data(
+        deltas_months,
+        title="Distribution of deal deltas (months)",
+        xlabel="Delta (months)",
+        ylabel="Frequency",
+        file_output_name="deals_deltas_raw.png"
+    )
+
+    plot.plot_std_data(
+        filtered_deltas,
+        title="Distribution of deal deltas (months) - Outliers removed",
+        xlabel="Delta (months)",
+        ylabel="Frequency",
+        file_output_name="deals_deltas_filtered.png"
+    )
+
--- plot.py
+++ plot.py
@@ -3,33 +3,31 @@ import plotly.graph_objects as go
 import statistics
 
 SECONDS_PER_MONTH = 60 * 60 * 24 * 30
+OUTPUT_PLOT_FOLDER = "./plots/"
 
 class Plot:
     def __init__(self):
         pass
 
-    def plot_std_data(self, deltas_months: list[float]):
-        if not deltas_months:
-            print("⚠️ Nessun dato disponibile per il plotting")
-            return
-
+    # png_generation: to enable pgn generation
+    def plot_std_data(self, deltas_months: list[float], title: str, xlabel: str, ylabel: str, png_generation: bool = False, file_output_name: str = ""):
         avg = statistics.mean(deltas_months)
         std = statistics.stdev(deltas_months) if len(deltas_months) > 1 else 0.0
 
         fig = go.Figure()
         fig.add_trace(go.Histogram(
             x=deltas_months,
-            name="Delta (mesi)",
+            name="Delta (months)",
             marker_color="lightblue",
             opacity=0.7,
-            xbins=dict(size=1)  # bucket di 1 mesi
+            xbins=dict(size=1)  # bucket size = 1
         ))
 
         fig.add_trace(go.Scatter(
             x=[avg, avg],
             y=[0, len(deltas_months)],
             mode="lines",
-            name="Media",
+            name="Avg",
             line=dict(color="red", dash="dash")
         ))
 
@@ -37,24 +35,27 @@ class Plot:
             x=[avg - std, avg - std],
             y=[0, len(deltas_months)],
             mode="lines",
-            name="Media - σ",
+            name="Avg - σ",
             line=dict(color="green", dash="dot")
         ))
         fig.add_trace(go.Scatter(
             x=[avg + std, avg + std],
             y=[0, len(deltas_months)],
             mode="lines",
-            name="Media + σ",
+            name="Avg + σ",
             line=dict(color="green", dash="dot")
         ))
 
         fig.update_layout(
-            title="Distribuzione dei delta tra i deal (in mesi)",
-            xaxis_title="Delta (mesi)",
-            yaxis_title="Frequenza",
+            title=title,
+            xaxis_title=xlabel,
+            yaxis_title=ylabel,
             bargap=0.2,
             template="plotly_white"
         )
+
+        if png_generation:
+            fig.write_image(f"{OUTPUT_PLOT_FOLDER}{file_output_name}")
 
         fig.show()
 
